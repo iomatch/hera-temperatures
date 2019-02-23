0e620bbe0e6fcf449c776e8db0fd01720d9cb0a4
Tommi Nikkila <tommi@oire.fi>
2019-02-23 11:47:49 +0200

    Stability update.

--- sbin/pushbullet.sh
+++ sbin/pushbullet.sh
@@ -1,11 +1,19 @@
-#/bin/bash
+#!/bin/bash
+# v.1.1.0
 # get your Access Token with the code from Pushbullet.com
 # and write it to etc/conf/pushbullet.sh.conf
 # create a link of this file to /usr/bin
 myNameIs=${0##*/}
 myPathIs=${0%/*}
-[ -z "$debug" ] && debug=0
-[ "$debug" -gt "0" ] && logger "$myNameIs :: $myPathIs/$myNameIs, \$debug: $debug"
+
+debug=1
+
+[ -z ${debug+x} ] && debug=0
+if [ "$debug" -gt "0" ]; then
+	logger -i " I am $myNameIs, the PushBullet shell script to send notifications. "
+fi
+#logger "$myNameIs :: $myPathIs/$myNameIs, \$debug: $debug"
+#logger "$myNameIs :: $myPathIs/$myNameIs, \$debug: $debug"
 baseName=$(basename $myNameIs)
 
 myDie() {
@@ -22,13 +30,14 @@ pbAPIkeyFile="/usr/local/etc/conf/"$baseName".conf"
 
 # Check if $pbAPIkeyFile is readable by user
 # and myDie() if not
+[ "$debug" -gt "0" ] && logger "$myNameIs :: Checking for file containing the API key..."
 if [ -r "$pbAPIkeyFile" ]; then
-	API=$(head -1 $pbAPIkeyFile|tr -d '[[:space:]]') || myDie "File ($pbAPIkeyFile) containing the Access Token needed for PushBullet cannot be read. Unable to continue."
+	API=$(head -1 $pbAPIkeyFile|tr -d '[[:space:]]') && logger "$myNameIs :: API key found." || myDie "File ($pbAPIkeyFile) containing the Access Token needed for PushBullet cannot be read. Unable to continue."
 else
 	myDie "Access Token key file ($pbAPIkeyFile) not found."
 fi
 
-# Check taht we have the Access Token as $API from $pbAPIkeyFile
+# Check that we have the Access Token as $API from $pbAPIkeyFile
 # or myDie() trying...
 [ -z "$API" ] && myDie "No Access Token found at $pbAPIkeyFile."
 
@@ -44,23 +53,19 @@ else
         title=$2
 fi
 
-#[ ! -z "$1" ] && curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="$title" -d body="$MSG" > /dev/null 2>&1 || echo "No message. Exiting."
+[ "$debug" -gt "0" ] && logger "$myNameIs :: Compiled push message:"
+[ "$debug" -gt "0" ] && logger "$myNameIs :: [ title ]: $title"
+[ "$debug" -gt "0" ] && logger "$myNameIs :: [  msg  ]: $MSG"
+
+sndDt="$(date +'%d.%m.%y %H:%M.%S')"
+
+MSG=$MSG" [log date: $sndDt]"
 if [ ! -z "$MSG" ]; then
-	if [ "$debug" -gt "0" ]; then
-		#logger "$myNameIs :: Sent PushBullet :: $MSG"
-		logger " "
-		logger "     pushpushpushpushpushpushpushpushpushpushpushpushpushpushpushpushpushpushpush"
-		logger "     u                                                                          h"
-		logger "     s  I am pushbullet.sh, the PushBullet shell script to send notifications.  s"
-		logger "     h                                                                          u"
-		logger "     hsuphsuphsuphsuphsuphsuphsuphsuphsuphsuphsuphsuphsuphsuphsuphsuphsuphsuphsup"
-		logger " "
-		logger "My message is:"
-		logger "$MSG"
-		logger " "
+	if curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="$title" -d body="$MSG" > /dev/null 2>&1; then
+		logger "$myNameIs :: Push notification sent successfully."
+	else
+		logger "$myNameIs :: Problem sending push notification."
 	fi
-	curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="$title" -d body="$MSG" > /dev/null 2>&1
 else
 	[ "$debug" -gt "0" ] && logger "$myNameIs :: No message, no PushBullet. Exiting."
-	#echo "No message. Exiting."
 fi
